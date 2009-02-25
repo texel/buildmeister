434285c0fd12d3dfc74c7f5ed0e123f1102660b5
Leigh Caplan <lcaplan@onehub.com>
2009-02-25 10:57:42 -0800

    Lots of rearranging deck chairs.

--- lib/buildmeister.rb
+++ lib/buildmeister.rb
@@ -4,16 +4,47 @@ require 'lighthouse'
 class Buildmeister
   VERSION = '1.0.0'
   
-  attr_accessor :project
+  attr_accessor :project, :project_name,
+                :ready, :staged, :verified,
+                :last_ready, :last_staged, :last_verified
   
   def initialize(project_name)
     @config = YAML.load_file(File.expand_path(File.dirname(__FILE__) + '/../config/config.yml'))
     Lighthouse.account = @config['account']
     Lighthouse.token   = @config['token']
-    self.get_project(project_name)
+    self.project_name = project_name
+    self.get_project
+        
+    self.load_project
   end
   
-  def get_project(project_name)
+  def load_project
+    bins  = self.get_project.bins
+    
+    self.ready     = bins.find { |bin| bin.name == 'Ready'     }
+    self.staged    = bins.find { |bin| bin.name == 'Staged'    }
+    self.verified  = bins.find { |bin| bin.name == 'Verified'  }
+  end
+  
+  def reload_info
+    self.last_ready, self.last_staged, self.last_verified = self.ready.tickets_count, self.staged.tickets_count, self.verified.tickets_count
+    
+    self.load_project
+    
+    # %w(ready staged verified).each do |state|
+    #   self.send(state).reload
+    # end
+    
+    self.ready.reload
+    self.staged.reload
+    self.verified.reload
+  end
+  
+  def changed?
+    [ready.tickets_count, staged.tickets_count, verified.tickets_count] != [last_ready, last_staged, last_verified]
+  end
+  
+  def get_project
     projects = Lighthouse::Project.find(:all)
     self.project  = projects.find {|pr| pr.name == project_name}
   end
--- lib/run_buildmeister.rb
+++ lib/run_buildmeister.rb
@@ -3,20 +3,19 @@ require 'activesupport'
 
 puts "Starting up BuildMeister..."
 
-while true do
   bm    = Buildmeister.new('Macchiato')
-  bins  = bm.project.bins
 
-  ready     = bins.find { |bin| bin.name == 'Ready'     }
-  staged    = bins.find { |bin| bin.name == 'Staged'    }
-  verified  = bins.find { |bin| bin.name == 'Verified'  }
-
+while true do  
   title = "BuildMeister: #{Time.now.strftime("%m/%d %I:%M %p")}"
-  body  = "Ready: #{ready.tickets.size}\nStaged: #{staged.tickets.size}\nVerified: #{verified.tickets.size}"
+  body  = "Ready: #{bm.ready.tickets_count}\nStaged: #{bm.staged.tickets_count}\nVerified: #{bm.verified.tickets_count}"
 
   puts "Updated notification at #{Time.now.strftime("%m/%d %I:%M %p")}"
-
-  `growlnotify -s -n "Lighthouse" -a "Lighthouse" -d 'buildmeister' -t "#{title}" -m "#{body}"`
   
-  sleep 10.minutes.to_i
+  if bm.changed?
+    `growlnotify -s -n "Lighthouse" -a "Lighthouse" -d 'buildmeister' -t "#{title}" -m "#{body}"`    
+  end
+  
+  sleep 5.minutes.to_i
+  
+  bm.reload_info
 end
