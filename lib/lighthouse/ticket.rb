795cd22e4351cf547d5f24abed90239ad3649392
Leigh Caplan <lcaplan@onehub.com>
2013-10-16 14:14:47 -0700

    Put state on the ticket.

--- lib/lighthouse/ticket.rb
+++ lib/lighthouse/ticket.rb
@@ -1,11 +1,12 @@
 module Lighthouse
   class Ticket
-    attr_reader :id, :name
+    attr_reader :id, :name, :state
 
     def initialize(attrs)
-      @id = attrs['number']      
-      @name = attrs['name']
-      @tag = attrs['tag']
+      @id    = attrs['number']
+      @name  = attrs['name']
+      @tag   = attrs['tag']
+      @state = attrs['state']
     end
 
     def tags
